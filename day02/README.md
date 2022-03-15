78c2698c858a8f33b685c1beff95cf2fe2d0997b
wangshuo <wangshuo@beagledata.com>
2022-03-15 17:39:33 +0800

    this完结

--- day02/README.md
+++ day02/README.md
@@ -48,3 +48,139 @@ f2() === undefined; // true
 ```
 
 ##### 类上下文
+
+> this 在类中的表现与函数相似，因为类本质上也是函数
+
+在构造函数中，类中所有非静态方法都会被添加到 this 原型中，静态方法不是 this 的属性，而是类自身的属性
+
+```js
+class Example {
+  constructor() {
+    const proto = Object.getPrototypeOf(this);
+    console.log(Object.getOwnPropertyNames(proto));
+  }
+  first() {}
+  second() {}
+  static third() {}
+}
+
+new Example(); // ['constructor', 'first', 'second']
+```
+
+##### 派生类
+
+> 派生类的函数默认是没有初始 this 的，在构造函数中调用`super`会生成一个 this，相当于执行以下代码,如果派生类在 super 之前调用会报错
+
+```js
+this = new Base();
+```
+
+##### this 和对象的转换
+
+在非严格模式下使用 call 或者 apply，如果使用 this 的值不是对象，则会尝试转换为对象,null,undefined 则会被转成全局对象`new Number(BoundThid) | String(BoundThid)`
+
+##### bind 方法
+
+> es5 引入了 Function.protoype.bind,调用`f.bind(someObject)`会创建一个与 f 具有相同体的函数和作用域函数，但是在这个函数中他的 this 被永久绑定在了 bind 的第一个参数，无论这个函数是如何被调用的，且 bind 只生效一次
+
+```js
+function f() {
+  return this.a;
+}
+
+var g = f.bind({a: "azerty"});
+console.log(g()); // azerty
+
+var h = g.bind({a: "yoo"}); // bind只生效一次！
+console.log(h()); // azerty
+
+var o = {a: 37, f: f, g: g, h: h};
+console.log(o.a, o.f(), o.g(), o.h()); // 37, 37, azerty, azerty
+```
+
+##### 箭头函数
+
+> 箭头函数中，this 与封闭词法环境的 this 保持一致，全局代码中，他被设置为全局 this
+
+##### 作为对象的方法
+
+> 当函数作为对象里的方法调用，this 指向调用函数的对象
+
+当`o.f()`调用时，函数内的 this 将绑定到 o 对象
+
+```js
+var o = {
+  prop: 37,
+  f: function () {
+    return this.prop;
+  },
+};
+
+console.log(o.f()); // 37
+```
+
+同样 this 的绑定直接收最近成员引用的影响
+
+```js
+o.b = {g: independent, prop: 42};
+console.log(o.b.g()); // 42
+```
+
+##### 原型链中的 this
+
+> 对于对象原型链上某处定义的方法，同样的概念也适用，如果该方法存在同一个原型链上，那么`this`指向的是调用这个方法的对象，就像该方法就这个对象上一样
+
+##### 作为构造函数
+
+> 当一个函数用作构造函数时（使用 new 关键字）他的 this 绑定到正在构造的新对象
+
+备注： 构造函数返回的默认值是 this 所指的那个对象，但是他仍然可以手动返回对象，**如果不是一个对象则返回 this 对象**
+
+```js
+/*
+ * 构造函数这样工作:
+ *
+ * function MyConstructor(){
+ *   // 函数实体写在这里
+ *   // 根据需要在this上创建属性，然后赋值给它们，比如：
+ *   this.fum = "nom";
+ *   // 等等...
+ *
+ *   // 如果函数具有返回对象的return语句，
+ *   // 则该对象将是 new 表达式的结果。
+ *   // 否则，表达式的结果是当前绑定到 this 的对象。
+ *   //（即通常看到的常见情况）。
+ * }
+ */
+
+function C() {
+  this.a = 37;
+}
+
+var o = new C();
+console.log(o.a); // logs 37
+
+function C2() {
+  this.a = 37;
+  return {a: 38};
+}
+
+o = new C2();
+console.log(o.a); // logs 38
+```
+
+##### 作为一个 dom 的事件处理函数
+
+> 当函数被作为 dom 事件处理函数时，他的 this 指向的是触发这个函数的元素
+
+##### 作为一个内联事件处理函数
+
+> 当函数被内联调用时，它的 this 指向监听器所在的 dom 元素
+
+##### 类中的 this
+
+> 和其他的普通函数一样，方法中的 this 值取决于他们如何调用，可以在构造函数（constructor）中让 this 的指向一直指向这个类的实例（bind）
+
+#### 总结
+
+this 是运行时绑定，在全局中 this 指向全局对象，在函数上下文中（函数体内）在非严格模式下 this 指向全局对象，在函数上下文中（函数体内）在严格模式下如果没有设置 this，this 的值为 undefined，在类的上下文中除了静态方法都会被添加到 this 上，派生类会在构造函数中会产生一个 this，如果在 super 之前调用 this 会报错，在非严格模式下 call，apply 如果 this 的值不是对象则会被转成对象 null 和 undefined 则会被转成 window，bind 将 this 永久绑定，且只生效一次，箭头函数的 this 与封闭词法环境的 this 保持一致，如果使用 call，apply，bind 去改变 this 的指向，this 不会被更改但是参数会被依次传递，对象方法的 this，会指向最近的调用对象，原型链中的 this 与对象中的方法一致，构造函数中的 this，如果构造函数返回值非对象则 this 为新构造的对象，如果构造函数返回的是引用则不会对外部有影响，dom 中的 this 指向触发该方法的 dom，类中的方法与对象中的方法一致，取决于他们被谁调用
--- day02/this.js
+++ day02/this.js
@@ -28,20 +28,239 @@
 // }
 
 // console.log(new Example()); // ['constructor', 'first', 'second']
-class Base {}
-class Good extends Base {}
-class AlsoGood extends Base {
-  constructor() {
-    return {a: 5};
-  }
-}
-class Bad extends Base {
-  constructor() {
-    super();
-    console.log(this);
-  }
-}
+// class Base {}
+// class Good extends Base {}
+// class AlsoGood extends Base {
+//   constructor() {
+//     return {a: 5};
+//   }
+//   add() {
+//     console.log(this);
+//   }
+// }
+// class Bad extends Base {
+//   constructor() {
+//     super();
+//     console.log(this);
+//   }
+// }
 
-new Good();
-new AlsoGood();
-new Bad(); // ReferenceError
+// new Good();
+// console.log(new AlsoGood().add());
+// new Bad(); // ReferenceError
+
+// class Base {
+//   constructor() {
+//     this.aa = 11;
+//   }
+// }
+// class Good {
+//   constructor() {
+//     this.aa = 12;
+//   }
+// }
+// const g = new Good();
+// console.log(g);
+
+// 对象可以作为 bind 或 apply 的第一个参数传递，并且该参数将绑定到该对象。
+// var obj = {a: "Custom"};
+
+// // 声明一个变量，并将该变量作为全局对象 window 的属性。
+// var a = "Global";
+
+// function whatsThis() {
+//   return this.a; // this 的值取决于函数被调用的方式
+// }
+
+// console.log(whatsThis()); // 'Global' 因为在这个函数中 this 没有被设定，所以它默认为 全局/ window 对象
+// console.log(whatsThis.call(obj)); // 'Custom' 因为函数中的 this 被设置为obj
+// console.log(whatsThis.apply(obj)); // 'Custom' 因为函数中的 this 被设置为obj
+// function bar() {
+//   console.log(Object.prototype.toString.call(this));
+// }
+
+// bar.call(7); // [object Number]
+// bar.call("foo"); // [object String]
+// bar.call(undefined); // [object global]
+// console.log(Object(undefined));
+
+// function f() {
+//   return this.a;
+// }
+
+// var g = f.bind({a: "azerty"});
+// console.log(g()); // azerty
+
+// var h = g.bind({a: "yoo"}); // bind只生效一次！
+// console.log(h()); // azerty
+
+// var o = {a: 37, f: f, g: g, h: h};
+// console.log(o.a, o.f(), o.g(), o.h()); // 37, 37, azerty, azerty
+
+// var globalObject = this;
+// var foo = () => this;
+// console.log(foo() === globalObject); // true
+// // 接着上面的代码
+// // 作为对象的一个方法调用
+// var obj = {foo: foo};
+// console.log(obj.foo() === globalObject); // true
+
+// // 尝试使用call来设定this
+// console.log(foo.call(obj) === globalObject); // true
+
+// // 尝试使用bind来设定this
+// foo = foo.bind(obj);
+// console.log(foo() === globalObject); // true
+
+// const obj = {
+//   aa: 11,
+//   bb: 22,
+//   add() {
+//     // console.log(this);
+//     (function () {
+//       console.log(this);
+//     })();
+//   },
+// };
+// obj.add();
+// const obj = {
+// //   a: 11,
+// //   b: 22,
+// // };
+// // const fn = (a, b) => {
+// //   console.log(this);
+// //   return a + b;
+// // };
+// // fn.call(null, 11, 22); // =>
+// // fn.apply(null, [11, 22]); // =>
+// // const fn1 = fn.bind(obj, 11, 22);
+// // fn1(); // =>
+// // fn1(33, 44); // =>
+// // const fn2 = fn1.bind(null, 33, 55);
+// // fn2(); // =>
+// // fn2(33, 44); // =>
+
+// // 创建一个含有bar方法的obj对象，
+// // bar返回一个函数，
+// // 这个函数返回this，
+// // 这个返回的函数是以箭头函数创建的，
+// // 所以它的this被永久绑定到了它外层函数的this。
+// // bar的值可以在调用中设置，这反过来又设置了返回函数的值。
+// // var obj = {
+// //   bar: function () {
+// //     var x = () => this;
+// //     return x;
+// //   },
+// // };
+
+// // 作为obj对象的一个方法来调用bar，把它的this绑定到obj。
+// // 将返回的函数的引用赋值给fn。
+// // var fn = obj.bar();
+
+// // 直接调用fn而不设置this，
+// // 通常(即不使用箭头函数的情况)默认为全局对象
+// // 若在严格模式则为undefined
+// // console.log(fn() === obj); // true
+
+// // 但是注意，如果你只是引用obj的方法，
+// // 而没有调用它
+// // var fn2 = obj.bar;
+// // 那么调用箭头函数后，this指向window，因为它从 bar 继承了this。
+// // console.log(fn2()() == window); // true
+// // "use strict";
+
+// // const fn = () => {
+// //   console.log(this);
+// // };
+// // fn();
+
+// // function sum() {
+// //   return this.a + this.b + this.c;
+// // }
+
+// // var o = {
+// //   a: 1,
+// //   b: 2,
+// //   c: 3,
+// //   get average() {
+// //     return (this.a + this.b + this.c) / 3;
+// //   },
+// // };
+
+// // Object.defineProperty(o, "sum", {
+// //   get: sum,
+// //   enumerable: true,
+// //   configurable: true,
+// // });
+
+// // console.log(o.average, o.sum); // logs 2, 6
+// // var a = 10;
+// // var b = 20;
+// // var o = {
+// //   f: function () {
+// //     return this.a + this.b;
+// //   },
+// // };
+// // var p = Object.create(o);
+// // p.a = 1;
+// // p.b = 4;
+
+// // const f = p.f;
+// // console.log(f());
+// // console.log(p.f()); // 5
+// // 被调用时，将关联的元素变成蓝色
+// // function bluify(e) {
+// //   console.log(this === e.currentTarget); // 总是 true
+
+// //   // 当 currentTarget 和 target 是同一个对象时为 true
+// //   console.log(this === e.target);
+// //   this.style.backgroundColor = "#A5D9F3";
+// // }
+
+// // // 获取文档中的所有元素的列表
+// // var elements = document.getElementsByTagName("*");
+
+// // // 将bluify作为元素的点击监听函数，当元素被点击时，就会变成蓝色
+// // for (var i = 0; i < elements.length; i++) {
+// //   elements[i].addEventListener("click", bluify, false);
+// // }
+
+// class Car {
+//   constructor() {
+//     // Bind sayBye but not sayHi to show the difference
+//     this.sayBye = this.sayBye.bind(this);
+//   }
+//   sayHi() {
+//     console.log(`Hello from ${this.name}`);
+//   }
+//   sayBye() {
+//     console.log(`Bye from ${this.name}`);
+//   }
+//   get name() {
+//     return "Ferrari";
+//   }
+// }
+
+// class Bird {
+//   get name() {
+//     return "Tweety";
+//   }
+// }
+
+// const car = new Car();
+// const bird = new Bird();
+
+// // The value of 'this' in methods depends on their caller
+// car.sayHi(); // Hello from Ferrari
+// bird.sayHi = car.sayHi;
+// bird.sayHi(); // Hello from Tweety
+
+// // For bound methods, 'this' doesn't depend on the caller
+// bird.sayBye = car.sayBye;
+// bird.sayBye(); // Bye from Ferrari
+
+function fn() {
+  "use strict";
+  console.log(this);
+}
+fn();
